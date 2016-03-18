5efefdda1174c6f988d447b5ac762a9008df79f4
Jasmine Pazer <jgp5980@rit.edu>
2016-03-18 12:47:23 -0400

    fixed a typo in app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var dbURL = process.envMONGOLAB_URI || "mongodb://localhost/DomoMaker";
+var dbURL = process.env.MONGOLAB_URI || "mongodb://localhost/DomoMaker";
 
 var db = mongoose.connect(dbURL, function (err) {
   if (err) {

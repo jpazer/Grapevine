bd492eb112f4daf4375a92d247f2737557c1eb51
jpazer <jgp5980@rit.edu>
2016-03-29 12:27:09 -0400

    Domomaker part c

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+var url = require('url');
+var csrf = require('csurf');
 
 var dbURL = process.env.MONGOLAB_URI || "mongodb://localhost/DomoMaker";
 
@@ -15,6 +18,18 @@ var db = mongoose.connect(dbURL, function (err) {
     throw err;
   }
 });
+
+var redisURL = {
+  hostname: 'localhost',
+  port: 6379
+};
+
+var redisPASS;
+
+if (process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  redisPASS = redisURL.auth.split(":")[1];
+}
 
 //pull in our routes
 var router = require('./router.js');
@@ -29,13 +44,32 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
   key: "sessionid",
+  store: new RedisStore({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    pass: redisPASS
+  }),
   secret: 'Domo Arigato',
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie:{
+    httpOnly: true
+  }
 }));
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
+app.disable('x-powered-by');
 app.use(cookieParser());
+//csrf must come after app.use(cookieParser());
+//and app.use(session({...});
+//and before the router
+app.use(csrf());
+app.use(function (err, req, res, next) {
+  if (err.code !== 'EBADCSRFTOKEN') {
+    return next(err);
+  }
+  return;
+});
 
 router(app);
 
